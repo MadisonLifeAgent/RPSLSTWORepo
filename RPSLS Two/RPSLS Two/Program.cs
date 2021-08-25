2dde76bec1082bacb0514f73f9c841350c752e40
Fue Moua <MadisonLifeAgent@gmail.com>
2021-08-25 18:55:07 -0500

    added StartEndGame class - which turns the game on or off

--- RPSLS Two/RPSLS Two/Program.cs
+++ RPSLS Two/RPSLS Two/Program.cs
@@ -8,25 +8,24 @@ namespace RPSLS_Two
         static void Main(string[] args)
         {
             // start the game
-            Game NewGame = new Game("", "");
+            StartEndGame NewGame = new StartEndGame();
+
+            //Debug line only
+            Console.WriteLine(NewGame.gameStatus);
 
             // create the players
-            HumanPlayer PlayerOne = new HumanPlayer("Player One");
-            HumanPlayer PlayerTwo = new HumanPlayer("Player Two");
-            ComputerPlayer ComputerPlayer = new ComputerPlayer("Computer Player");
+
 
 
             // Create list of options for RSPLS
-            List <Option> Options = new List<Option>();
+/*            List <Option> Options = new List<Option>();
                 Options.Add(new Option("rock"));
                 Options.Add(new Option("paper"));
                 Options.Add(new Option("scissors"));
                 Options.Add(new Option("lizard"));
-                Options.Add(new Option("spock"));
+                Options.Add(new Option("spock"));*/
 
             // ask for the number of human players
-            Console.WriteLine("Type 'one' to play against the CPU or 'two' to play against a friend");
-            NewGame.GetNumberOfHumanPlayers(Console.ReadLine());
 
             // get player selections            
         }
